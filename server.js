e2f10a564fdd93f5e361da4f3e6b088098c2afd3
Lavanya Natchiappan <lavee.natch@gmail.com>
2023-03-22 23:00:13 -0700

    try pushing note data

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
-const notesData = require('./db/db.json');
+const uuid = require('./helpers/uuid');
+const fs = require('fs');
+let notesData = require('./db/db.json')
 
 const PORT = 3001;
 
@@ -20,7 +22,55 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
   });
 
-app.get('/api/notes', (req, res) => res.json(notesData));
+app.get('/api/notes', (req, res) => {
+    console.log(notesData);
+    return res.json(notesData);
+});
+
+app.post('/api/notes', (req, res) => {
+    const {title, text } = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            review_id: uuid()
+        };
+
+        fs.readFile('./db/db.json', 'utf8', (err,data) => {
+            if (err) {
+                console.error(err);
+              } else {
+                // Convert string into JSON object
+                const parsedNotes = JSON.parse(data);
+        
+                // Add a new note
+                parsedNotes.push(newNote);
+        
+                // Write updated notes back to the file
+                fs.writeFile(
+                  './db/db.json',
+                  JSON.stringify(parsedNotes, null, 4),
+                  (writeErr) =>
+                    writeErr
+                      ? console.error(writeErr)
+                      : console.info('Successfully updated notes!')
+                );
+              }
+        });
+
+        const response = {
+            status: 'success',
+            body: newNote,
+          };
+      
+          console.log(response);
+          res.status(201).json(response);
+
+    } else {
+        res.status(500).json('Error in posting note');
+    }
+
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
